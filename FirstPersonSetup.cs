b7d06e15434fc27140d7db327f62fcc4344d0904
tanikeigo <foropudata@gmail.com>
2019-01-06 19:54:44 +0900

    MyVTuber
    non oculus sdk

---
+++ FirstPersonSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<VRM.VRMFirstPerson>().Setup();
+		foreach (var renderer in GetComponentsInChildren<SkinnedMeshRenderer>(true))
+		{
+			renderer.updateWhenOffscreen = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
